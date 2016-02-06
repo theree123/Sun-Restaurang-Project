ee68974f7c1e912ffa05d5fd3d1bd92fefee98d0
asun <asun.w@rmutsv.ac.th>
2016-02-06 13:35:34 +0700

    delete i can

--- app/src/main/java/th/co/rmutsv/sundaychillout/sun_restautran/MyManage.java
+++ app/src/main/java/th/co/rmutsv/sundaychillout/sun_restautran/MyManage.java
@@ -1,5 +1,6 @@
 package th.co.rmutsv.sundaychillout.sun_restautran;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -11,6 +12,16 @@ public class MyManage {
     private MyOpenHelper objopenHelper;
     private SQLiteDatabase writeSqLiteDatabase,readSqLiteDatabase;
 
+    public static final String user_TABLE = "userTABLE";
+    public static final String food_TABLE = "footTABLE";
+    public static final String colum_id = "_id";
+    public static final String colum_user = "User";
+    public static final String colum_pass = "Password";
+    public static final String colum_name = "Name";
+    public static final String colum_food = "foot";
+    public static final String colum_price = "price";
+    public static final String colum_source = "source";
+
     public MyManage(Context context){
 
         //cleate $ comment
@@ -18,5 +29,33 @@ public class MyManage {
         writeSqLiteDatabase = objopenHelper.getWritableDatabase();
         readSqLiteDatabase = objopenHelper.getReadableDatabase();
 
+    }
+    public long addNewValue(int inTable,
+                            String strColum2,
+                            String strColum3,
+                            String strColum4){
+        ContentValues obContentValues = new ContentValues();
+        long inReturn = 0;
+
+        switch (inTable) {
+            case 0:
+
+                obContentValues.put(colum_user, strColum2);
+                obContentValues.put(colum_pass, strColum3);
+                obContentValues.put(colum_name, strColum4);
+                writeSqLiteDatabase.insert(user_TABLE, null, obContentValues);
+
+                break;
+            case 1:
+
+                obContentValues.put(colum_food, strColum2);
+                obContentValues.put(colum_price, strColum3);
+                obContentValues.put(colum_source, strColum4);
+                writeSqLiteDatabase.insert(food_TABLE, null, obContentValues);
+
+                break;
+        }//switch
+
+        return inReturn;
     }
 } //main classic
--- app/src/main/java/th/co/rmutsv/sundaychillout/sun_restautran/SundayChillOut.java
+++ app/src/main/java/th/co/rmutsv/sundaychillout/sun_restautran/SundayChillOut.java
@@ -1,5 +1,6 @@
 package th.co.rmutsv.sundaychillout.sun_restautran;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,7 +16,32 @@ public class SundayChillOut extends AppCompatActivity {
         setContentView(R.layout.activity_sunday_chill_out);
         //object
         obMyManage=new MyManage(this);
+
+        //tes add value
+        //tesAddvalue();
+
+        //Clean data
+        cleanData();
+
+
     } //main mettord
+
+    private void cleanData() {
+
+        SQLiteDatabase obSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_APPEND, null);
+        obSqLiteDatabase.delete(MyManage.food_TABLE, null,null);
+        obSqLiteDatabase.delete(MyManage.user_TABLE, null,null);
+
+    }//cleanData
+
+    private void tesAddvalue() {
+
+        for (int i = 0; i <= 1; i++) {
+            obMyManage.addNewValue(i,"tast1", "test2", "test3");
+        }//for
+
+    }   //testaddvalue
 
 }   //mainclassic
 
