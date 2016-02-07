c05a162eecfd6feb9478aa80801696507e706994
asun <asun.w@rmutsv.ac.th>
2016-02-07 13:19:28 +0700

    Desk Sprinner แล้ว

--- app/src/main/java/th/co/rmutsv/sundaychillout/sun_restautran/ServiceActivity.java
+++ app/src/main/java/th/co/rmutsv/sundaychillout/sun_restautran/ServiceActivity.java
@@ -3,6 +3,9 @@ package th.co.rmutsv.sundaychillout.sun_restautran;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -31,8 +34,30 @@ public class ServiceActivity extends AppCompatActivity {
 
         showView();
 
+        showDesk();
+
 
     } //main method
+
+    private void showDesk() {
+
+        final String[] showDeskStrings = {"โต๊ะที่ 1", "โต๊ะที่ 2", "โต๊ะที่ 3", "โต๊ะที่ 4",
+                "โต๊ะที่ 5", "โต๊ะที่ 6", "โต๊ะที่ 7", "โต๊ะที่ 8", "โต๊ะที่ 9", "โต๊ะที่ 10"};
+        ArrayAdapter<String> deskAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, showDeskStrings);
+        deskSpinner.setAdapter(deskAdapter);
+        deskSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                deskString = showDeskStrings[i];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                deskString = showDeskStrings[0];
+            }
+        });
+    } //Show Desk
 
     private void showView() {
 
